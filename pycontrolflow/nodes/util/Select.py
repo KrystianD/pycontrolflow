60f3cbd6b9b5774e011a9bbddc7f270970132a4a
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-01-06 14:25:30 +0100

    Select: accept node input

--- pycontrolflow/nodes/util/Select.py
+++ pycontrolflow/nodes/util/Select.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta
 from typing import Any, TypeVar, Optional, Tuple, List
 
-from pycontrolflow.flow_value import FlowValue
+from pycontrolflow.flow_value import FlowValue, resolve_value
 from pycontrolflow.nodes.FlowSingleOutputNode import FlowSingleOutputNode
+from pycontrolflow.types import TNodeInput
 
 TOutput = TypeVar("TOutput")
 
@@ -11,15 +12,18 @@ class Select(FlowSingleOutputNode[TOutput]):
     def __init__(self) -> None:
         super().__init__()
 
-        self.conditions: List[Tuple[FlowValue[bool], Optional[TOutput]]] = []
+        self.conditions: List[Tuple[FlowValue[bool], TNodeInput]] = []
         self.has_default = False
-        self.default_value: Optional[TOutput] = None
+        self.default_value: TNodeInput = None
 
-    def case(self, cond1: FlowValue[bool], value1: Optional[TOutput]) -> 'Select[TOutput]':
+    def case(self, cond1: TNodeInput[bool], value1: TNodeInput[TOutput]) -> 'Select[TOutput]':
+        self._register_provider(cond1)
+        self._register_provider(value1)
         self.conditions.append((cond1, value1))
         return self
 
-    def default(self, value: Optional[TOutput]) -> 'Select[TOutput]':
+    def default(self, value: TNodeInput[TOutput]) -> 'Select[TOutput]':
+        self._register_provider(value)
         self.has_default = True
         self.default_value = value
         return self
@@ -27,9 +31,9 @@ class Select(FlowSingleOutputNode[TOutput]):
     def process(self, cur_date: datetime, delta: timedelta) -> None:
         super().process(cur_date, delta)
         for cond, out_value in self.conditions:
-            if cond.get() is True:
-                self.set_output(out_value)
+            if resolve_value(cond) is True:
+                self.set_output(resolve_value(out_value))
                 return
 
         if self.has_default:
-            self.set_output(self.default_value)
+            self.set_output(resolve_value(self.default_value))
