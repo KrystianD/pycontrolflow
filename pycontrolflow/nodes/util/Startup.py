8ad7a466fa864e61536074dc5c86e4b3dee939a6
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-03-14 03:11:29 +0100

    added Startup node

---
+++ pycontrolflow/nodes/util/Startup.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+from pycontrolflow.nodes.FlowSingleOutputNode import FlowSingleOutputNode
+
+
+class Startup(FlowSingleOutputNode[bool]):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.first_run = True
+
+    def process(self, cur_date: datetime, delta: timedelta) -> None:
+        prev = self.first_run
+        self.first_run = False
+
+        self.set_output(prev)
