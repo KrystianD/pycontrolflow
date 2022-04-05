494abc9915727faabe754dc985ad7b9400fa2a52
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-04-05 02:13:27 +0200

    single output type check

--- pycontrolflow/nodes/FlowSingleOutputNode.py
+++ pycontrolflow/nodes/FlowSingleOutputNode.py
@@ -1,6 +1,6 @@
 import typing
 from abc import ABC
-from typing import TypeVar, Generic, Iterable, Optional, Any, Type
+from typing import TypeVar, Iterable, Optional, Any, Type
 
 from pycontrolflow.IFlowValueProvider import IFlowValueProvider
 from pycontrolflow.flow_value import FlowValue
@@ -9,14 +9,17 @@ from pycontrolflow.nodes.FlowNode import FlowNode
 TFlowSingleOutputNodeType = TypeVar("TFlowSingleOutputNodeType")
 
 
-class FlowSingleOutputNode(Generic[TFlowSingleOutputNodeType], FlowNode, IFlowValueProvider[TFlowSingleOutputNodeType], ABC):
+class FlowSingleOutputNode(FlowNode, IFlowValueProvider[TFlowSingleOutputNodeType], ABC):
     def __init__(self, providers: Optional[Iterable[Any]] = None, nid: Optional[str] = None) -> None:
         super().__init__(providers, nid=nid)
         self.output_value: Optional[FlowValue[TFlowSingleOutputNodeType]] = None
 
     def to(self, output_value: FlowValue[TFlowSingleOutputNodeType]) -> 'FlowSingleOutputNode[TFlowSingleOutputNodeType]':
-        if output_value.get_type() != self.get_type():
-            raise TypeError("output type does not match expected type (bool)")
+        node_type = self.get_type()
+        output_value_type = output_value.get_type()
+
+        if output_value_type != node_type:
+            raise TypeError(f"output type does not match expected type, got: {output_value_type}, expected: {node_type}")
         self.output_value = output_value
         return self
 
@@ -28,8 +31,12 @@ class FlowSingleOutputNode(Generic[TFlowSingleOutputNodeType], FlowNode, IFlowVa
         assert self.output_value is not None
         return self.output_value.get()
 
+    # noinspection PyUnresolvedReferences
+    # noinspection PyProtectedMember
     def get_type(self) -> Type[TFlowSingleOutputNodeType]:
-        for v in self.__orig_bases__:  # type: ignore
-            if isinstance(v, typing._GenericAlias):  # type: ignore
-                return typing.get_args(v)[0]  # type: ignore
+        if hasattr(self, "__orig_class__"):
+            return typing.get_args(getattr(self, "__orig_class__"))[0]
+        for v in self.__orig_bases__:
+            if isinstance(v, typing._GenericAlias):
+                return typing.get_args(v)[0]
         assert False
