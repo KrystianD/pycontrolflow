4640cb64f24aa84d5391731fed67fc71d9adc1f4
Krystian Dużyński <krystian.duzynski@gmail.com>
2022-11-09 18:56:13 +0100

    Changed: improved tests

--- pycontrolflow/nodes/comparators/Changed_test.py
+++ pycontrolflow/nodes/comparators/Changed_test.py
@@ -3,24 +3,30 @@ from datetime import datetime
 
 from pycontrolflow.FlowExecutor import FlowExecutor
 from pycontrolflow.nodes.comparators.Changed import Changed
-from pycontrolflow.nodes.comparators.Compare import CompareGreaterThan
 
 
 class ChangedTest(unittest.TestCase):
     def test(self) -> None:
         executor = FlowExecutor()
 
-        in1 = executor.var("test1", float, default=1)
-
+        in1 = executor.memory("test1", int, initial_value=1)
         out = executor.var("out", bool)
 
         executor.add([
-            Changed[float](in1).to(out),
+            Changed[int](in1).to(out),
         ])
 
+        in1.set(1)
         executor.run(datetime(2020, 1, 1, 15, 0, 00))
-
         self.assertFalse(out.get())
+
+        in1.set(1)
+        executor.run(datetime(2020, 1, 1, 15, 1, 00))
+        self.assertFalse(out.get())
+
+        in1.set(2)
+        executor.run(datetime(2020, 1, 1, 15, 2, 00))
+        self.assertTrue(out.get())
 
 
 if __name__ == '__main__':
