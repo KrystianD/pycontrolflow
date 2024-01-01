c9bd4012ffc5f62947fa9d76566df3efc92a8f81
Krystian Dużyński <krystian.duzynski@gmail.com>
2024-01-01 19:54:19 +0100

    hidden timer and enabled variables in FlowTimerOneShot

--- src/pycontrolflow/FlowTimerOneShot.py
+++ src/pycontrolflow/FlowTimerOneShot.py
@@ -4,6 +4,7 @@ from typing import Optional
 from pycontrolflow.FlowExecutor import FlowExecutor
 from pycontrolflow.FlowTimer import FlowTimer
 from pycontrolflow.flow_executor_helpers import var_for_node, memory_for_node
+from pycontrolflow.read_only_flow_value import ReadOnlyFlowValue
 
 
 class FlowTimerOneShot(FlowTimer):
@@ -17,33 +18,36 @@ class FlowTimerOneShot(FlowTimer):
         self._persistent = persistent
 
         self.trigger = var_for_node(flow_executor, nid, "trigger", bool, False)
-        self.timer = memory_for_node(flow_executor, nid, "duration", timedelta, timedelta(), persistent=persistent)
-        self.enabled = memory_for_node(flow_executor, nid, "enabled", bool, False, persistent=persistent)
+        self._timer = memory_for_node(flow_executor, nid, "duration", timedelta, timedelta(), persistent=persistent)
+        self._enabled = memory_for_node(flow_executor, nid, "enabled", bool, False, persistent=persistent)
+
+        self.timer = ReadOnlyFlowValue(self._timer)
+        self.enabled = ReadOnlyFlowValue(self._enabled)
 
     def _trigger(self) -> None:
-        if self.enabled.get():
+        if self._enabled.get():
             if self._extend_on_trigger:
-                self.timer.set(timedelta())  # reset timer
+                self._timer.set(timedelta())  # reset timer
         else:
-            self.timer.set(timedelta())  # reset timer
-            self.enabled.set(True)
+            self._timer.set(timedelta())  # reset timer
+            self._enabled.set(True)
 
     def stop(self) -> None:
-        self.timer.set(timedelta())  # reset timer
-        self.enabled.set(False)
+        self._timer.set(timedelta())  # reset timer
+        self._enabled.set(False)
 
     def start_cycle(self) -> None:
         pass
 
     def process(self, delta: timedelta) -> None:
-        if self.enabled.get():
-            cur_duration = self.timer.get()
+        if self._enabled.get():
+            cur_duration = self._timer.get()
             assert cur_duration is not None
-            self.timer.set(cur_duration + delta)
+            self._timer.set(cur_duration + delta)
 
-            if self.timer.get() >= self._duration:
-                self.enabled.set(False)
-                self.timer.set(self._duration)
+            if self._timer.get() >= self._duration:
+                self._enabled.set(False)
+                self._timer.set(self._duration)
 
     def process_after(self) -> None:
         if self.trigger.get() is True:
